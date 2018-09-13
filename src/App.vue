de815ad614b3058f244d83287ebdb7ff2aa87116
Tom Bonner <tom+github@bonner.is>
2018-09-13 13:22:59 +0100

    Added vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <Tutorial v-bind:markdown="this.markdown" />
+  </div>
+</template>
+
+<script>
+
+import {Tutorial} from "./Tutorial.vue"
+
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Loading Data",
+      markdownUrl: "https://raw.githubusercontent.com/Glitch0011/CodingWithTom/master/data.mk",
+      markdown: null
+    };
+  },
+  components: {
+      Tutorial
+  },
+  mounted: async function() {
+      this.msg = "Loading";
+
+      let request = await fetch(this.markdownUrl);
+      let response = await request.text();
+
+      this.msg = "Loaded";
+      this.markdown = response;
+  }
+};
+</script>
+
+<style lang="css">
+#app {
+  color: #56b983;
+}
+</style>
---
+++ src/Tutorial.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tutorial",
+  data() {
+    return {
+      msg: "Loading Data",
+      markdownUrl: "https://raw.githubusercontent.com/Glitch0011/CodingWithTom/master/data.mk"
+    };
+  },
+  mounted: async function() {
+      this.msg = "Loading";
+
+      let request = await fetch(this.markdownUrl);
+      let response = await request.text();
+
+      this.msg = "Loaded";
+  }
+};
+</script>
+
+<style lang="css">
+#app {
+  color: #56b983;
+}
+</style>
