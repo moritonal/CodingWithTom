3954c054de30d39ebc387e71b9482b74d6e9cfe1
Tom Bonner <tom+github@bonner.is>
2018-09-18 12:35:05 +0100

    Fixed Loading object slightly

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
-    <div v-if="this.state == 'LOADING'">
-        <h1>Loading</h1>
-    </div>
     <div v-if="this.state == 'SHOW'">
         <Tutorial v-bind:markdown="this.markdown" />
     </div>
+    <transition name="sweep" duration="500">
+      <div v-bind:class="{fullPageOverlay : true}" v-show="this.state == 'LOADING'">
+        <h1>Loading</h1>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -41,8 +43,30 @@ export default {
 
 <style lang="css">
 
+.fullPageOverlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  
+  background-color: rgb(0,0,0,0.4);
+  border: 1px solid rgb(0,0,0,0.4s);
+  border-radius: 10px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 #app {
 
+}
+
+html {
+  overflow-y: scroll;
 }
 
 </style>
