9df72b56b37039014f04df540ea6828db9fdfd1b
Tom Bonner <tom+github@bonner.is>
2018-09-24 16:35:57 +0100

    Updates

--- scss.436ac0ca.js
+++ scss.436ac0ca.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '27879' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '3157' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
